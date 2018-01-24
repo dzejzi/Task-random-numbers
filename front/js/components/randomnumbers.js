6bcca3e55f90e54762d53d80b139282c24e418c8
dzejzi <justynazubala@gmail.com>
2018-01-24 01:06:27 +0100

    working latest numbers

--- front/js/components/randomnumbers.js
+++ front/js/components/randomnumbers.js
@@ -1,19 +1,32 @@
-fetch('http://localhost:3000/random-numbers', {
-        method: 'GET',
-      })
-      .then(response => response.json())
-      .then(responseJson =>{
+document.addEventListener('DOMContentLoaded', function(){
 
-          let secondCard = document.querySelectorAll('.card')[1];
-          let newUl = document.createElement('ul');
-          newUl.classList.add('list-group');
-          newUl.classList.add('list-group-flash');
+let secondCard = document.querySelectorAll('.card')[1];
+let newUl = document.createElement('ul');
+newUl.classList.add('list-group');
+newUl.classList.add('list-group-flash');
 
-          responseJson.data.forEach((element)=>{
-              let newLi = document.createElement('li');
-              newLi.classList.add('list-group-item');
-              newLi.innerHTML = element;
-              secondCard.append(newLi);
-          });
-      })
-      .catch(error => console.error('Error:', error));
+
+function addRandomNumbersTOSecondCard() {
+    setInterval(() => {
+        fetch('http://localhost:3000/random-numbers', {
+                method: 'GET',
+            })
+            .then(response => response.json())
+            .then(responseJson => {
+                newUl.innerHTML = '';
+                responseJson.data.forEach((element) => {
+                    let newLi = document.createElement('li');
+                    newLi.classList.add('list-group-item');
+                    newLi.innerHTML = element;
+                    newUl.append(newLi);
+                    secondCard.append(newUl);
+                });
+
+            })
+            .catch(error => console.error('Error:', error));
+    }, 1000)
+}
+
+addRandomNumbersTOSecondCard();
+
+})
